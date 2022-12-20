2bbd65885e6fbd526f7bc7e8810f908a040426b6
shea7073 <shea7073@gmail.com>
2022-12-19 23:22:41 -0500

    winter break is here

--- myJournal/my_posts/views.py
+++ myJournal/my_posts/views.py
@@ -10,18 +10,7 @@ class PostDayArchiveView(DayArchiveView):
     date_field = 'post_date'
     month_format = '%m'
 
-    # def get_context_data(self, **kwargs):
-    #     context = super().get_context_data(**kwargs)
-    #     obj= Post.objects.filter(owner=self.request.user).latest('post_date')
-    #     field_object = Post._meta.get_field('post_date')
-    #     field_value = field_object.value_from_object(obj)
-    #     year = field_value.year
-    #     day = field_value.day
-    #     month = field_value.month
-    #     context["day"] = day
-    #     context['month'] = month
-    #     context['year'] = year
-    #     return context
+    extra_context
 
     def get_queryset(self):
         return Post.objects.filter(owner=self.request.user)
