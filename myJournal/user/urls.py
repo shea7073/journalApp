e1f072b90f2530fea177ddceb22b16e264248368
shea7073 <shea7073@gmail.com>
2023-01-05 22:55:49 -0500

    started login styles

--- myJournal/user/urls.py
+++ myJournal/user/urls.py
@@ -3,6 +3,6 @@ from . import views
 
 
 urlpatterns = [
-    path('', views.login_user, name='login'),
+    path('login', views.login_user, name='login'),
     path('logout_user/', views.logout_user, name='logout'),
 ]
